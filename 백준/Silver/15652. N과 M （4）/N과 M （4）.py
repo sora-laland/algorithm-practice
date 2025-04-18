64cfa2ff3beee0dbc908d07cdaa372acc2fd3cd2
Lee Sora <156387338+sora-laland@users.noreply.github.com>
2025-04-19 00:03:35 +0900

    [Silver III] Title: N과 M (4), Time: 56 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+arr = list(range(1, N+1))
+path = []
+
+# 조합
+def repeated_combi(x):
+
+    if x == M:
+        print(*path)
+        return
+
+    for num in arr:
+        # 직전에 뽑은 요소가 지금보다 크면 넘겨
+        # 중복이 허용되므로 같아도 됨
+        if path and num < path[-1]:
+            continue
+            
+        path.append(num)
+        repeated_combi(x+1)
+        path.pop()
+
+repeated_combi(0)
+
